314a95d4fee99e6f335157ead3d5f0bbbba53ef0
furkan0325 <furkanali0325@gmail.com>
2023-07-24 15:10:22 +0530

    print 10 by recursion

---
+++ src/com/test/coding/Print10ByRecursion.java
@@ -0,0 +1,18 @@
+package com.test.coding;
+
+public class Print10ByRecursion {
+    public static void main(String[] args) {
+        printbyrecursion(10);
+    }
+
+    private static void printbyrecursion(int n) {
+        int i=1;
+        if(n==1)
+            System.out.println(n);
+        else{
+            System.out.println(i++);
+            printbyrecursion(n-1);
+        }
+
+    }
+}
