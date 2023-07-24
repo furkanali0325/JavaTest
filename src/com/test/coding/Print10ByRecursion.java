e1851a19db900482aafeeb1096c8bc5d7d9d649e
furkanali0325 <62588631+furkanali0325@users.noreply.github.com>
2023-07-24 15:11:48 +0530

    Update Print10ByRecursion.java

--- src/com/test/coding/Print10ByRecursion.java
+++ src/com/test/coding/Print10ByRecursion.java
@@ -7,8 +7,10 @@ public class Print10ByRecursion {
 
     private static void printbyrecursion(int n) {
         int i=1;
-        if(n==1)
+        if(n==1){
+            System.out.println("you can also add something that you need");
             System.out.println(n);
+        }
         else{
             System.out.println(i++);
             printbyrecursion(n-1);
